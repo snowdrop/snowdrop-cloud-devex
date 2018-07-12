64cd671e7b080e5a7957e925516dabd0e28f4976
Chris Laprun <metacosm@gmail.com>
2018-07-12 16:39:00 +0200

    Use latest version of Spring Boot HTTP booster as base.

--- spring-boot/src/main/java/me/snowdrop/service/GreetingEndpoint.java
+++ spring-boot/src/main/java/me/snowdrop/service/GreetingEndpoint.java
@@ -13,23 +13,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.openshift.booster.service;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
 import org.springframework.stereotype.Component;
 
+@Path("/greeting")
 @Component
-@ConfigurationProperties("greeting")
-public class GreetingProperties {
-
-    private String message = "Hello, %s!";
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
+public class GreetingEndpoint {
+    @GET
+    @Produces("application/json")
+    public Greeting greeting(@QueryParam("name") @DefaultValue("World") String name) {
+        final String message = String.format(Greeting.FORMAT, name);
+        return new Greeting(message);
     }
 }
