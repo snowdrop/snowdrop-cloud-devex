4d499ae9038cb470fbbb0e64603445e7b92a81a5
Chris Laprun <metacosm@gmail.com>
2018-07-23 11:37:50 +0200

    Remove troublesome init state recording.

--- cmd/init.go
+++ cmd/init.go
@@ -4,23 +4,10 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
-	"encoding/json"
 	"github.com/openshift/api/apps/v1"
 	"github.com/snowdrop/k8s-supervisor/pkg/buildpack"
 	"github.com/snowdrop/k8s-supervisor/pkg/buildpack/types"
-	"io/ioutil"
-	"os"
 )
-
-const stateFileName = ".sb.state"
-
-type state struct {
-	Finished bool `json:"finished"`
-	Is       bool `json:"is"`
-	Pvc      bool `json:"pvc"`
-	Svc      bool `json:"svc"`
-	Route    bool `json:"route"`
-}
 
 var initCmd = &cobra.Command{
 	Use:     "init [flags]",
@@ -31,74 +18,29 @@ var initCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 
 		log.Info("Init command called")
-		log.Debugf("Namespace: %s", namespace)
+		setup := Setup()
 
-		// check if we have an existing record of the init state
-		var state = &state{}
-		if _, err := os.Stat(stateFileName); err == nil {
-			source, err := ioutil.ReadFile(stateFileName)
-			if err != nil {
-				panic(err)
-			}
-
-			err = json.Unmarshal(source, state)
-			if err != nil {
-				log.Fatalf("Couldn't read %s init state file, restarting from scratch", stateFileName)
-			}
+		// Create ImageStreams
+		log.Info("Create ImageStreams for Supervisord and Java S2I Image of SpringBoot")
+		images := []types.Image{
+			*buildpack.CreateTypeImage(true, "dev-s2i", "latest", "quay.io/snowdrop/spring-boot-s2i", false),
+			*buildpack.CreateTypeImage(true, "copy-supervisord", "latest", "quay.io/snowdrop/supervisord", true),
 		}
+		buildpack.CreateImageStreamTemplate(setup.RestConfig, setup.Application, images)
 
-		// only init if we haven't done it already
-		if !state.Finished {
-			setup := Setup()
+		// Create PVC
+		log.Info("Create PVC to store m2 repo")
+		buildpack.CreatePVC(setup.Clientset, setup.Application, "1Gi")
 
-			if !state.Is {
-				// Create ImageStreams
-				images := []types.Image{
-					*buildpack.CreateTypeImage(true, "dev-s2i", "latest", "quay.io/snowdrop/spring-boot-s2i", false),
-					*buildpack.CreateTypeImage(true, "copy-supervisord", "latest", "quay.io/snowdrop/supervisord", true),
-				}
+		var dc *v1.DeploymentConfig
+		log.Info("Create or retrieve DeploymentConfig using Supervisord and Java S2I Image of SpringBoot")
+		dc = buildpack.CreateOrRetrieveDeploymentConfig(setup.RestConfig, setup.Application)
 
-				buildpack.CreateImageStreamTemplate(setup.RestConfig, setup.Application, images)
-				log.Info("Create ImageStreams for Supervisord and Java S2I Image of SpringBoot")
-				state.Is = true
-			} else {
-				log.Info("ImageStreams already created")
-			}
+		log.Info("Create Service using Template")
+		buildpack.CreateServiceTemplate(setup.Clientset, dc, setup.Application)
 
-			if !state.Pvc {
-				// Create PVC
-				log.Info("Create PVC to store m2 repo")
-				buildpack.CreatePVC(setup.Clientset, setup.Application, "1Gi")
-				state.Pvc = true
-			} else {
-				log.Info("PVC already created")
-			}
-
-			var dc *v1.DeploymentConfig
-			log.Info("Create or retrieve DeploymentConfig using Supervisord and Java S2I Image of SpringBoot")
-			dc = buildpack.CreateOrRetrieveDeploymentConfig(setup.RestConfig, setup.Application)
-
-			if !state.Svc {
-				log.Info("Create Service using Template")
-				buildpack.CreateServiceTemplate(setup.Clientset, dc, setup.Application)
-				state.Svc = true
-			} else {
-				log.Info("Service already created")
-			}
-
-			if !state.Route {
-				log.Info("Create Route using Template")
-				buildpack.CreateRouteTemplate(setup.RestConfig, setup.Application)
-				state.Route = true
-			} else {
-				log.Info("Route already created")
-			}
-
-			bytes, _ := json.Marshal(state)
-			ioutil.WriteFile(stateFileName, bytes, 0644)
-		} else {
-			log.Info("init already done")
-		}
+		log.Info("Create Route using Template")
+		buildpack.CreateRouteTemplate(setup.RestConfig, setup.Application)
 	},
 }
 
