79df04d0c536af49fe9123ba518863e9380ecf3a
Charles Moulliard <cmoulliard@redhat.com>
2018-08-09 19:33:31 +0200

    Command works to generate simple app

--- pkg/scaffold/scaffold.go
+++ pkg/scaffold/scaffold.go
@@ -12,19 +12,22 @@ import (
 	log "github.com/sirupsen/logrus"
 	"bytes"
 	"archive/zip"
+	"io"
+	"github.com/gobuffalo/packr"
 )
 
 const (
 	trimPrefix   = "tmpl/"
-	templateDir  = "/tmpl/"
-	generatedDir = "./generated/"
+	templateDir  = "./tmpl/"
+	outDir       = "/generated/"
 	javaPattern  = "*.java"
 	pomPattern   = "pom.xml"
 )
 
 var (
-	files []string
-	templates = make(map[string]template.Template)
+	files       []string
+	templates   = make(map[string]template.Template)
+	box 		= packr.NewBox(templateDir)
 )
 
 type Project struct {
@@ -38,19 +41,30 @@ type Project struct {
 }
 
 func GenerateProjectFiles(p Project) {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	// Generate pom.xml
-	collectTemplates(packageDirectory() + templateDir, pomPattern)
-	parseTemplates(p)
+	collectBoxTemplates()
+	parseTemplates(currentDir,"/",p)
 
-	// Generate Java files
-	collectTemplates(packageDirectory() + templateDir, javaPattern)
-    parseTemplates(p)
-
-	PopulateZip()
+	//PopulateZip()
 
 	fmt.Println("Done !!")
 }
 
+func collectBoxTemplates() {
+	log.Infof("List of files :",box.List())
+	for _, tmpl:= range box.List() {
+		log.Debug("File : " + tmpl)
+
+		t := template.New(tmpl)
+		t, _ = t.Parse(box.String(tmpl))
+		templates[tmpl] = *t
+	}
+}
 func collectTemplates(dir string, pattern string) {
 	filepath.Walk(dir, visitFile(pattern))
 
@@ -61,7 +75,7 @@ func collectTemplates(dir string, pattern string) {
 			log.Fatal(err)
 		}
 		name := strings.TrimPrefix(f, trimPrefix)
-		log.Debug("Template File Name :", name)
+		log.Info("Template File Name :", name)
 
 		t := template.New(name)
 		t, _ = t.Parse(string(b))
@@ -88,30 +102,73 @@ func visitFile(pattern string) filepath.WalkFunc {
 		return nil
 	}
 }
-func parseTemplates(p Project) {
-	var b bytes.Buffer
+func parseTemplates(dir string, outDir string, p Project) {
 	for _, t := range templates {
 		log.Debug("##### Template : ", t.Name())
+		var b bytes.Buffer
 		err := t.Execute(&b, p)
 		if err != nil {
 			fmt.Println("There was an error:", err.Error())
 		}
 		log.Debug("##### Generated : ", b.String())
-		os.MkdirAll(generatedDir + path.Dir(t.Name()), os.ModePerm);
-		err = ioutil.WriteFile(generatedDir + t.Name(), b.Bytes(),0644)
+		os.MkdirAll(dir + outDir + path.Dir(t.Name()), os.ModePerm);
+		err = ioutil.WriteFile(dir + outDir + t.Name(), b.Bytes(),0644)
 		if err != nil {
 			fmt.Println("There was an error:", err.Error())
 		}
 	}
 }
-
 func PopulateZip() {
-	// Create a buffer to write our archive to.
-	buf := new(bytes.Buffer)
+	createExampleFiles()
+
+	zipFile, err := os.Create("generated.zip")
+	if err != nil {
+		//
+	}
+	defer zipFile.Close()
 
 	// Create a new zip archive.
-	w := zip.NewWriter(buf)
+	w := zip.NewWriter(zipFile)
+	defer w.Close()
 
+
+	f, err := os.Open("readme.txt")
+	if err != nil {
+		//
+	}
+	defer f.Close()
+
+	// Get the file information
+	info, err := f.Stat()
+	if err != nil {
+		//
+	}
+
+	header, err := zip.FileInfoHeader(info)
+	if err != nil {
+		//
+	}
+
+	// Change to deflate to gain better compression
+	// see http://golang.org/pkg/archive/zip/#pkg-constants
+	header.Method = zip.Deflate
+
+	writer, err := w.CreateHeader(header)
+	if err != nil {
+		//
+	}
+	_, err = io.Copy(writer, f)
+	if err != nil {
+		//
+	}
+
+	// Make sure to check the error on Close.
+	errClose := w.Close()
+	if errClose != nil {
+		log.Fatal(err)
+	}
+}
+func createExampleFiles() {
 	// Add some files to the archive.
 	var files = []struct {
 		Name, Body string
@@ -121,25 +178,14 @@ func PopulateZip() {
 		{"todo.txt", "Get animal handling licence.\nWrite more examples."},
 	}
 	for _, file := range files {
-		f, err := w.Create(file.Name)
+		err := ioutil.WriteFile(file.Name,[]byte(file.Body),0644)
 		if err != nil {
 			log.Fatal(err)
 		}
-		_, err = f.Write([]byte(file.Body))
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	// Make sure to check the error on Close.
-	err := w.Close()
-	if err != nil {
-		log.Fatal(err)
 	}
 }
-
 func packageDirectory() string {
-	_, filename, _, ok := runtime.Caller(0)
+	_, filename, _, ok := runtime.Caller(1)
 	if !ok {
 		panic("No caller information")
 	}
