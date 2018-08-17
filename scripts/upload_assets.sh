cb4591bb922764897ab4563bc42e4bf9452f219d
Charles Moulliard <cmoulliard@redhat.com>
2018-08-17 08:55:17 +0200

    Pass version as parameter

--- scripts/upload_assets.sh
+++ scripts/upload_assets.sh
@@ -17,41 +17,47 @@ GH_REPO="$GH_API/repos/$owner/$repo"
 WGET_ARGS="--content-disposition --auth-no-challenge --no-cookie"
 CURL_ARGS="-LJO#"
 
-TAG="v0.3.0"
+TAG="v$version"
 GH_TAGS="$GH_REPO/releases/tags/$TAG"
 
 BIN_DIR="./dist/bin/"
 RELEASE_DIR="./dist/release"
 APP="sb"
 
+#echo "Create Release"
 JSON='{"tag_name": "'"$TAG"'","target_commitish": "master","name": "'"$TAG"'","body": "'"$TAG"'-release","draft": false,"prerelease": false}'
 
-#echo "Create Release"
-#curl -H "$AUTH" \
-#     -H "Content-Type: application/json" \
-#     -d "$JSON" \
-#     $GH_REPO/releases
+curl -H "$AUTH" \
+     -H "Content-Type: application/json" \
+     -d "$JSON" \
+     $GH_REPO/releases
 
 # Read asset tags.
-# response=$(curl -sH "$AUTH" $GH_TAGS)
-#
-# # Get ID of the asset based on given filename.
-# eval $(echo "$response" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
-# [ "$id" ] || { echo "Error: Failed to get release id for tag: $tag"; echo "$response" | awk 'length($0)<100' >&2; exit 1; }
-# echo "ID : $id"
-#
-# # upload assets
-# for arch in `ls -1 $BIN_DIR/`;do
-#     suffix=""
-#     if [[ $arch == windows-* ]]; then
-#         suffix=".exe"
-#     fi
-#     target_file=$RELEASE_DIR/$APP-$arch$suffix.tar.gz
-#     content_type=$(file -b --mime-type $target_file)
-#
-#     GH_ASSET="https://uploads.github.com/repos/$owner/$repo/releases/$id/assets?name=$(basename $target_file)"
-#
-#     curl -i -H "Authorization: token $github_api_token" \
-#             -H "Content-Type: application/gzip" \
-#             -d @$target_file $GH_ASSET
-# done
+response=$(curl -sH "$AUTH" $GH_TAGS)
+
+# Get ID of the asset based on given filename.
+eval $(echo "$response" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
+[ "$id" ] || { echo "Error: Failed to get release id for tag: $tag"; echo "$response" | awk 'length($0)<100' >&2; exit 1; }
+echo "ID : $id"
+
+# Upload assets
+for arch in `ls -1 $BIN_DIR/`;do
+     suffix=""
+     if [[ $arch == windows-* ]]; then
+         suffix=".exe"
+     fi
+     target_file=$RELEASE_DIR/$APP-$arch$suffix.tgz
+     content_type=$(file -b --mime-type $target_file)
+     echo "Asset : $target_file"
+     echo "Content type : $content_type"
+
+     GH_ASSET_URL="https://uploads.github.com/repos/$owner/$repo/releases/$id/assets?name=$(basename $target_file)"
+     echo "GH_ASSET_URL : $GH_ASSET_URL"
+
+     curl -H "Authorization: token $github_api_token" \
+          -H "Accept: application/vnd.github.v3+json" \
+          -H "Content-Type: $content_type" \
+          -X POST \
+          --data-binary @$target_file \
+          $GH_ASSET_URL
+done
