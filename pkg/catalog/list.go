8eea3c9942bfed4b642b818761ddb4170d51741c
Georgios Andrianakis <geoand@gmail.com>
2018-08-28 12:21:14 +0300

    Show results of sb catalog list in a properly formatted table (#45)
    Fixes: #42

--- pkg/catalog/list.go
+++ pkg/catalog/list.go
@@ -1,19 +1,18 @@
 package catalog
 
 import (
+	"encoding/json"
+	scv1beta1 "github.com/kubernetes-incubator/service-catalog/pkg/apis/servicecatalog/v1beta1"
+	servicecatalogclienset "github.com/kubernetes-incubator/service-catalog/pkg/client/clientset_generated/clientset/typed/servicecatalog/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	restclient "k8s.io/client-go/rest"
-
-	servicecatalogclienset "github.com/kubernetes-incubator/service-catalog/pkg/client/clientset_generated/clientset/typed/servicecatalog/v1beta1"
-	scv1beta1 "github.com/kubernetes-incubator/service-catalog/pkg/apis/servicecatalog/v1beta1"
+	"os"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
 
+	"github.com/olekukonko/tablewriter"
 	"github.com/pkg/errors"
-)
-
-const (
-	BROKER_NAME = "openshift-automation-service-broker"
 )
 
 func List(config *restclient.Config) {
@@ -22,20 +21,41 @@ func List(config *restclient.Config) {
 	log.Info("List of services")
 	log.Info("================")
 
-	for  _, class  := range classes {
-		log.Infof("ID : %s, Class description : %s", class.Name, class.Spec.Description)
-		metaData, _ := PrettyPrint(class.Spec.ExternalMetadata.Raw)
-		log.Infof("Additional info : %s",metaData)
+	printResults(classes)
+}
 
-		log.Info("---------------------------------------------------")
+func printResults(classes []scv1beta1.ClusterServiceClass) {
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Name", "Description", "Long Description"})
+	table.SetAlignment(tablewriter.ALIGN_LEFT)
+	for _, class := range classes {
+		var meta map[string]interface{}
+		json.Unmarshal(class.Spec.ExternalMetadata.Raw, &meta)
+		longDescription := "--------"
+		if val, ok := meta["longDescription"]; ok {
+			longDescription = val.(string)
+		}
+		row := []string{class.Spec.ExternalName, class.Spec.Description, longDescription}
+		table.Append(row)
 	}
+	table.Render()
 }
 
 // GetClusterServiceClasses queries the service service catalog to get available clusterServiceClasses
 func getClusterServiceClasses(scc *servicecatalogclienset.ServicecatalogV1beta1Client) ([]scv1beta1.ClusterServiceClass, error) {
-	classList, err := scc.ClusterServiceClasses().List(metav1.ListOptions{FieldSelector: "spec.clusterServiceBrokerName=" + BROKER_NAME})
+	classList, err := scc.ClusterServiceClasses().List(metav1.ListOptions{})
 	if err != nil {
 		return nil, errors.Wrap(err, "unable to list cluster service classes")
 	}
-	return classList.Items, nil
+	itemsFromAutomationBroker := make([]scv1beta1.ClusterServiceClass, 0)
+	for _, class := range classList.Items {
+		// we are only interested in services from the automation-service-broker
+		// this broker however could have various names depending on the setup
+		if strings.Contains(class.Spec.ClusterServiceBrokerName, "automation") &&
+			strings.Contains(class.Spec.ClusterServiceBrokerName, "broker") {
+
+			itemsFromAutomationBroker = append(itemsFromAutomationBroker, class)
+		}
+	}
+	return itemsFromAutomationBroker, nil
 }
